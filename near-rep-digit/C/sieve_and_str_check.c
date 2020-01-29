c9701904c71d10c5a7800fa4d6baffc1ea86f5c4
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-01-29 22:27:27 +0530

    Upgrade C code to 64 bit

--- near-rep-digit/C/sieve_and_str_check.c
+++ near-rep-digit/C/sieve_and_str_check.c
@@ -5,18 +5,17 @@
 #include<math.h>
 #include<time.h>
 
-int size = 5;
-int current_slot = 0;
-int *primes_arr;
+unsigned long size = 5, current_slot = 0;
+unsigned long long* primes_arr;
 
-void append(int data)
+void append(unsigned long long data)
 {
     if ((current_slot + 1) < size)
     {
         primes_arr[current_slot++] = data;
         return;
     }
-    int* newarr = realloc(primes_arr, (size+=5) * sizeof(int));
+    unsigned long long* newarr = realloc(primes_arr, (size += 5l) * sizeof(unsigned long long));
     if (newarr != NULL)
     {
         newarr[current_slot++] = data;
@@ -29,12 +28,18 @@ void append(int data)
     }
 }
 
-bool isNearRep(int num)
+int countDigits(unsigned long long num)
 {
-    int i, unqcharcount = 0;
-    char str[100], commonchar;      // The str can hold a max of 100 digits - change if needed
-    snprintf(str, 100, "%d", num);  // Converting the integer number to a string
-    if (strlen(str) < 3)
+    return snprintf(NULL, 0, "%llu", num) - (num < 0);
+}
+
+bool isNearRep(unsigned long long num)
+{
+    int i, unqcharcount = 0, digitcount = countDigits(num);
+    char* str, commonchar;
+    str = malloc((digitcount + 1) * sizeof(char));
+    snprintf(str, digitcount + 1, "%llu", num);    // Converting the integer number to a string
+    if (digitcount < 3)
     {
         // Near Rep numbers should be at least 3 digits
         return false;
@@ -56,7 +61,7 @@ bool isNearRep(int num)
         // if no common character is found i.e 968999 or 988999
         return false;
     }
-    /** 
+    /**
     * The above conditional statements will still find a common char
     * even if the number is 99986574, this is why we loop through for
     * additional checks
@@ -86,30 +91,27 @@ bool isNearRep(int num)
 
 // The following is just a standard approach to segmented sieve, nothing interesting
 
-void simpleSieve(int limit)
+void simpleSieve(unsigned long limit)
 {
-    int p;
+    unsigned long long p;
     if (primes_arr == NULL)
     {
         printf("\nAn error occured while allocating primes_arr for mark in simpleSieve\n");
         exit(1);
     }
     bool* mark = malloc((limit + 1) * sizeof(bool));
-    if (mark != NULL)
+    if (mark == NULL)
     {
-        memset(mark, true, sizeof(bool) * (limit + 1));
-    }
-    else
-    {
-        printf("\nAn error occured while allocating memory for mark in simpleSieve\n");
+        printf("\nAn error occured while allocating memory for mark in segmentedSieve\n");
         exit(1);
     }
+    memset(mark, true, sizeof(bool) * (limit + 1));
 
     for (p = 2; p * p < limit; p++)
     {
         if (mark[p])
         {
-            for (int i = p * 2; i < limit; i += p)
+            for (unsigned long long i = p * 2; i < limit; i += p)
             {
                 mark[i] = false;
             }
@@ -123,19 +125,19 @@ void simpleSieve(int limit)
             append(p);
             if (isNearRep(p))
             {
-                printf("%d ", p);
+                printf("%llu, ", p);
             }
         }
     }
 }
 
-void segmentedSieve(int n)
+void segmentedSieve(unsigned long long n)
 {
-    int limit = (int)floor(sqrt(n)) + 1;
+    unsigned long limit = (unsigned long)floor(sqrt(n)) + 1l;
     simpleSieve(limit);
 
-    int low = limit;
-    int high = 2 * limit;
+    unsigned long low = limit;
+    unsigned long high = 2 * limit;
 
     while (low < n)
     {
@@ -144,33 +146,31 @@ void segmentedSieve(int n)
             high = n;
         }
         bool* mark = malloc((limit + 1) * sizeof(bool));
-        if (mark != NULL)
-        {
-            memset(mark, true, sizeof(bool) * (limit + 1));
-        }
-        else
+        if (mark == NULL)
         {
             printf("\nAn error occured while allocating memory for mark in segmentedSieve\n");
             exit(1);
         }
-        for (int i = 0; i < current_slot; i++)
+        memset(mark, true, sizeof(bool) * (limit + 1));
+
+        for (unsigned long long i = 0; i < current_slot; i++)
         {
-            int lower_lim = (int)floor(low / primes_arr[i]) * primes_arr[i];
+            unsigned long long lower_lim = (unsigned long)floor(low / primes_arr[i]) * primes_arr[i];
             if (lower_lim < low)
             {
                 lower_lim += primes_arr[i];
             }
-            for (int j = lower_lim; j < high; j += primes_arr[i])
+            for (unsigned long long j = lower_lim; j < high; j += primes_arr[i])
             {
                 mark[j - low] = false;
             }
         }
 
-        for (int i = low; i < high; i++)
+        for (unsigned long long i = low; i < high; i++)
         {
-            if (mark[i - low] == true && isNearRep(i))
+            if (mark[i - low] && isNearRep(i))
             {
-                printf("%d ", i);
+                printf("%llu, ", i);
             }
         }
         low = low + limit;
@@ -181,15 +181,17 @@ void segmentedSieve(int n)
 
 int main()
 {
-    int N;
+    unsigned long long N;
     double time_taken;
-    primes_arr = malloc(size * sizeof(int));
+    primes_arr = malloc(size * sizeof(unsigned long long));
     printf("Enter upper limit: ");
-    scanf_s("%d", &N);
+    scanf_s("%llu", &N);
     clock_t begin = clock();
+    printf("[");
     segmentedSieve(N);
+    printf("\b\b]");
     clock_t end = clock();
-    time_taken = (double) (end - begin) / CLOCKS_PER_SEC;
+    time_taken = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("\nDone! Exectuion time: %f\n", time_taken);
     return 0;
 }
--- near-rep-digit/README.md
+++ near-rep-digit/README.md
@@ -4,10 +4,12 @@
 * `sieve_and_regex` : It uses a standard segmented [Sieve of Eratosthenes](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes) approach and then 2 **regex expressions** to check if the prime number is also a near rep digit prime. This is the *second fastest* method here
 
 # Usage
+**Makes sure to use the programs in 64 bit if you plan to compute bigger numbers**
+
 * `non_sieve_and_unique_set.py` : Simply run `python non_sieve_and_unique_set.py` or `python3 non_sieve_and_unique_set`
 * `sieve_and_str_check.py` : Simply run `python sieve_and_str_check.py` or `python3 sieve_and_str_check`
 * `sieve_and_regex.py` : Simply run `python sieve_and_str_check.py` or `python3 sieve_and_str_check`
-* `sieve_and_str_check.c` : You can either compile using `MSVC` or `clang` and run the generated executable file
+* `sieve_and_str_check.c` : You can either compile using `MSVC` or `clang` and run the generated executable file - **Must be compiled in 64 bit**
 
 # SPEEEEED!
 
@@ -27,8 +29,8 @@ The `python` script for `sieve_and_str_check` completes in **8.7 seconds** (best
 
 And now, for the **fastest of them all**, `sieve_and_str_check` in `C`. This is *really* damn fast.
 
-The `C` program for `sieve_and_str_check` completes in **0.2 seconds** (best case scenario)
+The `C` program for `sieve_and_str_check` completes in **0.4 seconds** (best case scenario)
 
-![image](https://user-images.githubusercontent.com/44284917/73118657-007ee700-3f7d-11ea-855f-34ae3936615f.png)
+![image](https://user-images.githubusercontent.com/44284917/73377608-51783d80-42e5-11ea-9290-4a4b3ec8223a.png)
 
-yes, 0.2 seconds. `C` straight up destroys `python` in this one.
+yes, ~0.4 seconds. `C` straight up destroys `python` in this one.
